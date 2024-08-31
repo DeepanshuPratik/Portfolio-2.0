f071382b994e7ff6dff6966fbdf53f0f52f9cf10
DeepanshuPratik <deepanshu.pratik@gmail.com>
2024-09-01 01:07:33 +0530

    setup: setting up navigation section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
-const jetbrainsMono = JetBrains_Mono({ 
-  subsets: ["latin"], 
-  weight: ["100","200","300","400","500","600","700","800"],
-  variable: "--font-jetbrainsMono"
- });
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-jetbrainsMono",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,7 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={jetbrainsMono.variable}>{children}</body>
+      <body className={jetbrainsMono.variable}>
+        <Header/>
+        {children}
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,58 +23,30 @@ const config: Config = {
 		primary: "var(--font-jetbrainsMono)"
 	},
   	extend: {
-  		backgroundImage: {
-  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {
-  			// background: 'hsl(var(--background))',
-  			// foreground: 'hsl(var(--foreground))',
-  			// card: {
-  			// 	DEFAULT: 'hsl(var(--card))',
-  			// 	foreground: 'hsl(var(--card-foreground))'
-  			// },
-  			// popover: {
-  			// 	DEFAULT: 'hsl(var(--popover))',
-  			// 	foreground: 'hsl(var(--popover-foreground))'
-  			// },
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				// foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			// secondary: {
-  			// 	DEFAULT: 'hsl(var(--secondary))',
-  			// 	foreground: 'hsl(var(--secondary-foreground))'
-  			// },
-  			// muted: {
-  			// 	DEFAULT: 'hsl(var(--muted))',
-  			// 	foreground: 'hsl(var(--muted-foreground))'
-  			// },
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-				hover: 'hsl(var(--accent-hover))'
-  				// foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			// destructive: {
-  			// 	DEFAULT: 'hsl(var(--destructive))',
-  			// 	foreground: 'hsl(var(--destructive-foreground))'
-  			// },
-  			// border: 'hsl(var(--border))',
-  			// input: 'hsl(var(--input))',
-  			// ring: 'hsl(var(--ring))',
-  			// chart: {
-  			// 	'1': 'hsl(var(--chart-1))',
-  			// 	'2': 'hsl(var(--chart-2))',
-  			// 	'3': 'hsl(var(--chart-3))',
-  			// 	'4': 'hsl(var(--chart-4))',
-  			// 	'5': 'hsl(var(--chart-5))'
-  			// }
-  		}
+		colors: {
+			primary: 'var(--primary)',
+			'primary-foreground': 'var(--primary-foreground)',
+			secondary: 'var(--secondary)',
+			'secondary-foreground': 'var(--secondary-foreground)',
+			muted: 'var(--muted)',
+			'muted-foreground': 'var(--muted-foreground)',
+			accent: 'var(--accent)',
+			'accent-hover': 'var(--accent-hover)',
+			'accent-foreground': 'var(--accent-foreground)',
+			destructive: 'var(--destructive)',
+			'destructive-foreground': 'var(--destructive-foreground)',
+			border: 'var(--border)',
+			input: 'var(--input)',
+			ring: 'var(--ring)',
+			chart1: 'var(--chart-1)',
+			chart2: 'var(--chart-2)',
+			chart3: 'var(--chart-3)',
+			chart4: 'var(--chart-4)',
+			chart5: 'var(--chart-5)',
+		  },
+		  borderRadius: {
+			DEFAULT: 'var(--radius)',
+		  },
   	}
   },
   plugins: [require("tailwindcss-animate")],
