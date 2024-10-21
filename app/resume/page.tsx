86ca39b1c74716b6c5dfff9e01b186f9b4cc94db
DeepanshuPratik <deepanshu.pratik@gmail.com>
2024-10-21 16:49:50 +0530

    feat: Resume page experience and education designed

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -70,19 +70,22 @@ const education = {
   description: 'Lorem ipsum 2',
   items: [
     {
-      institution: '',
-      degree: '',
-      duration: ''
+      institution: 'IIIT Naya Raipur',
+      degree: 'B. Tech',
+      duration: '2020-2024',
+      grade: '9.1/10'
     },
     {
-      institution: '',
-      degree: '',
-      duration: ''
+      institution: 'Vivekanand School',
+      degree: '12th',
+      duration: '2018-2019',
+      grade:'89%'
     },
     {
-      institution: '',
-      degree: '',
-      duration: ''
+      institution: 'Vivekanand School',
+      degree: '10th',
+      duration: '2016-2017',
+      grade:'9.4/10'
     }
   ]
 }
@@ -98,26 +101,123 @@ const skills = {
       name: "HTML 5"
     },
     {
-      icon: <FaHtml5 />,
-      name: "HTML 5"
+      icon: <FaFigma />,
+      name: "Figma"
     },
     {
-      icon: <FaHtml5 />,
-      name: "HTML 5"
+      icon: <FaNodeJs />,
+      name: "Node JS"
     },
     {
-      icon: <FaHtml5 />,
-      name: "HTML 5"
+      icon: <FaReact />,
+      name: "React"
     }
   ]
 }
 
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+
 const Resume = () => {
   return (
-    <div>
-      Resume Page
-    </div>
-  )
+    <motion.div initial={{opacity:0}} 
+    animate={{
+      opacity:1,
+      transition: { delay: 2.4, duration: 0.4, ease:"easeIn"},
+    }}
+    className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs defaultValue="experience" className="flex flex-col xl:flex-row gap-[60px]">
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:ms-0 gap-6">
+            <TabsTrigger value={"experience"}>Experience</TabsTrigger>
+            <TabsTrigger value={"education"}>Education</TabsTrigger>
+            <TabsTrigger value={"skills"}>Skills</TabsTrigger>
+            <TabsTrigger value={"about"}>About Me</TabsTrigger>
+          </TabsList>
+
+          <div className="min-h-[70vh] w-full">
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/70 mx-auto xl:mx-0">{experience.description}</p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {experience.items.map((item,index)=> {
+                      return(
+                        <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.position}</h3>
+                          <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] rounded-full"></span>
+                            <p className="text-white/70">{item.company}</p>
+                          </div>
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div> 
+            </TabsContent>
+            <TabsContent value="education" className="w-full">
+            <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{education.title}</h3>
+                <p className="max-w-[600px] text-white/70 mx-auto xl:mx-0">{education.description}</p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {education.items.map((item,index)=> {
+                      return(
+                        <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.degree}</h3>
+                          <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] rounded-full"></span>
+                            <p className="text-white/70">{item.institution}</p>
+                          </div>
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>  
+            </TabsContent>
+            <TabsContent value="skills" className="w-full h-full">
+              <div className="flex flex-col gap-[30px]">
+                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                  <h3 className="text-4xl font-bold">{skills.title}</h3>
+                  <p className="max-w-[600px] text-white/70 mx-auto xl:mx-0">{skills.description}</p>
+                  <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:gap-[30px]">
+                    {skills.skillList.map((skill, index)=>{
+                      return <li key={index}>
+                        <TooltipProvider delayDuration={100}>
+                          <Tooltip>
+                            <TooltipTrigger>
+                              <div className="text-6xl group-hover:text-accent transition-all duration-300">{skill.icon}</div>
+                            </TooltipTrigger>
+                            <TooltipContent>{skill.name}</TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </li>;
+                    })}
+                  </ul>
+                </div>
+              </div> 
+            </TabsContent>
+            <TabsContent value="about" className="w-full">
+              About Me 
+            </TabsContent>
+            
+          </div>
+        </Tabs>
+      </div>
+
+    </motion.div>
+  );
 }
 
 export default Resume
