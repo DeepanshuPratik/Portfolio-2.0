944c1607b08fa0b9e2f25ec21fa2c09cd61ce008
pallavi-sahu <pallavisahu0610@gmail.com>
2024-10-30 14:27:52 +0530

    Calling modal carousel component in the service page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { BsArrowDownRight } from 'react-icons/bs';
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import Link from 'next/link';
+import { useState } from 'react';
+import CustomModal from '@/components/ui/modal';
 
 const services = [
   {
@@ -31,39 +33,61 @@ const services = [
 ]
 
 const Services = () => {
-  return <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
-    <div className="container mx-auto">
-      <motion.div
-      initial={{ opacity: 0}}
-      animate={{
-        opacity: 1,
-        transition: { delay: 2.4, duration: 0.4, ease:"easeIn"}, 
-      }}
-      className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
-      >
-        {services.map((service, index) => {
-          return (
-            <div key={index} className="flex-1 flex flex-col justify-center gap-6 group">
-              <div className="w-full flex justify-between items-center">
-                <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
-                  {service.num}
-                </div>
-                <Link href={service.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"> 
-                  <BsArrowDownRight className="text-primary text-3xl" />
-                </Link>
-              </div>
-              <h2 className='text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500'>
-                {service.title}
-              </h2>
-              <p className='text-white/70'>{service.description}</p>
-              <div className="border-b border-white/20 w-full"></div>
-            </div>
-          );
-        })}
+  const [isModalOpen, setModalOpen] = useState<boolean>(false);
 
-      </motion.div>
-    </div>
-  </section>;
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
+  return <>
+    <CustomModal
+      open={isModalOpen}
+      onClose={handleCloseModal}
+      images={[
+        "https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547_960_720.jpg",
+        "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg",
+        "https://cdn.pixabay.com/photo/2024/10/17/16/14/waterfall-9128051_960_720.jpg"
+      ]}
+    >
+    </CustomModal>
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+      <div className="container mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+          }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+        >
+          {services.map((service, index) => {
+            return (
+              <div key={index} className="flex-1 flex flex-col justify-center gap-6 group">
+                <div className="w-full flex justify-between items-center">
+                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
+                    {service.num}
+                  </div>
+                  <Link onClick={handleOpenModal} href={service.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
+                    <BsArrowDownRight className="text-primary text-3xl" />
+                  </Link>
+                </div>
+                <h2 className='text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500'>
+                  {service.title}
+                </h2>
+                <p className='text-white/70'>{service.description}</p>
+                <div className="border-b border-white/20 w-full"></div>
+              </div>
+            );
+          })}
+
+        </motion.div>
+      </div>
+    </section>
+  </>;
 };
 
 export default Services;
