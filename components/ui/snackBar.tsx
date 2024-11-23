a0975f13b923c6e7bcb8d1dde42bd773415316d5
pallavi-sahu <pallavisahu0610@gmail.com>
2024-11-24 01:08:15 +0530

    snackbar implementation

---
+++ components/ui/snackBar.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Alert, Snackbar } from "@mui/material";
+
+interface SnackbarProps {
+  message: string;
+  open: boolean;
+  handleClose?: () => void;
+  severity?: "error" | "warning" | "info" | "success";
+  duration?: number;
+}
+
+const CustomSnackbar: React.FC<SnackbarProps> = ({
+  message,
+  open,
+  handleClose,
+  severity = "info",
+  duration = 5000,
+}) => {
+  return (
+    <Snackbar
+      open={open}
+      autoHideDuration={duration}
+      onClose={handleClose}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }} 
+    >
+      <Alert
+        onClose={handleClose}
+        severity={severity}
+        variant="filled"
+        sx={{
+          width: "100%",
+          fontSize: "1rem",
+        }}
+      >
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
+
+export default CustomSnackbar;
